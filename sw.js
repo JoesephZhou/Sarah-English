2dcb90e7c22e83d3038ce2d6adf40ce8b03d5977
JoesephZhou <final2hiya2018@gmail.com>
2026-03-24 20:40:24 -0500

    Updates

---
+++ sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>{self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
